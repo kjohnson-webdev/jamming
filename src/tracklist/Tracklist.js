1dbb966b66e9c58644e3e94af82b1a93a073bd92
Quarl88 <k.j.johnson88@gmail.com>
2023-10-16 15:46:25 -0700

    feat(Tracklist): format track duration

--- src/tracklist/Tracklist.js
+++ src/tracklist/Tracklist.js
@@ -4,6 +4,13 @@ import mockTracks from '../SpotifyUtility';
 
 function Tracklist() {
     const tracks = mockTracks;
+
+    function formatDuration(duration_ms) {
+        const minutes = Math.floor((duration_ms/1000)/60);
+        const seconds = Math.floor((duration_ms/1000) % 60);
+        return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`
+    }
+
     return (
         <div>
             <h2>Tracklist</h2>
@@ -13,7 +20,7 @@ function Tracklist() {
                     name={track.name}
                     artist={track.artists[0].name}
                     album={track.album.name}
-                    duration={track.duration_ms}
+                    duration={formatDuration(track.duration_ms)}
                 />
             ))}
         </div>
